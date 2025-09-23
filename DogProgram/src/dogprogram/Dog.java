40fcaf905637ebfa8c2272e16d4d21f8cc9eb08d
weir809 <benweir809@gmail.com>
2025-09-23 11:11:48 +0100

    Dog Program - week 3

---
+++ DogProgram/src/dogprogram/Dog.java
@@ -0,0 +1,26 @@
+package dogprogram;
+
+public class Dog {
+	
+	//Instant variables
+	String name;
+	char gender;
+	
+	//methods
+	public void bark(){
+		
+		System.out.println("Woof");
+		
+			}
+	
+	
+		public void detailDisplay() {
+			
+			System.out.println("Details for "+ name);
+			System.out.println("Gender: " + gender);
+		
+		}
+	
+	
+
+}
---
+++ DogProgram/src/dogprogram/DogProgram.java
@@ -0,0 +1,17 @@
+package dogprogram;
+
+public class DogProgram {
+	
+	public static void main(String[] args) {
+		Dog theDog = new Dog() ;
+
+		
+		theDog.name = "Frodo";
+		theDog.gender = 'M' ;
+		
+	
+		theDog.bark();
+		theDog.detailDisplay();
+	}
+
+}
